bb24502c2216c5d1cc960bb220079a3081b6038b
khethelo7 <saneleknkosi@gmail.com>
2023-03-10 16:48:27 +0200

    set up the size of the world

--- world/pygame/world.py
+++ world/pygame/world.py
@@ -2,7 +2,7 @@ import pygame
 import os
 
 #border of world
-width, height = 200, 400
+width, height = 900, 900
 
 #color declaration constants
 SILVER = (192, 192, 192)
@@ -17,3 +17,28 @@ FPS = 60
 #robot declaration constants
 ROBO_WIDTH, ROBO_HEIGHT = 40, 40
 
+#world declaration constants
+WIN = pygame.display.set_mode((width, height))
+
+def setup_world():
+    WIN.fill(BLUE)
+
+
+def main():
+    
+    run = True
+    clock = pygame.time.Clock()
+    
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+        setup_world()
+    
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
