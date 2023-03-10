ca2d0a0ee154056e30bdd7a66edcf8b0c166daaf
khethelo7 <saneleknkosi@gmail.com>
2023-03-10 16:39:44 +0200

    Added pygame file for world

---
+++ world/pygame/world.py
@@ -0,0 +1,19 @@
+import pygame
+import os
+
+#border of world
+width, height = 200, 400
+
+#color declaration constants
+SILVER = (192, 192, 192)
+BLUE = (50, 80, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+#graphics declaration constants
+FPS = 60
+
+#robot declaration constants
+ROBO_WIDTH, ROBO_HEIGHT = 40, 40
+
