e0e41c97e3340be7d8b7714177a11f162cbb4420
khethelo7 <saneleknkosi@gmail.com>
2023-03-14 13:03:41 +0200

    Obstacles finally render dynamically, to refactor number of obstacles

--- world/pygame/world.py
+++ world/pygame/world.py
@@ -13,7 +13,7 @@ RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
 # graphics declaration constants
-FPS = 60
+FPS = 30
 VOID = pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'void.png')), (width, height))
 
 # robot declaration constants
@@ -33,7 +33,39 @@ ASTEROID_IMAGE = pygame.transform.scale(pygame.image.load(
     os.path.join('Assets', 'asteroid.png')), (OBS_WIDTH, OBS_HEIGHT))
 
 # variables recieved from robot.py
-obstacles_list = []
+asteroids = []
+
+
+class Asteroid:
+    def __init__(self, x, y, velocity, image):
+        self.x = x
+        self.y = y
+        self.velocity = velocity
+        self.image = image
+        self.width = OBS_WIDTH
+        self.height = OBS_HEIGHT
+
+def create_asteroid():
+    global asteroids
+    
+    x = random.randint(0, width - OBS_WIDTH)
+    y = 0 - OBS_HEIGHT
+    velocity = random.randint(1, VEL)
+    asteroid = Asteroid(x, y, velocity, ASTEROID_IMAGE)
+    asteroids.append(asteroid)
+    
+
+def move_asteroids():
+    global asteroids
+    
+    for i in range(1):
+        create_asteroid()
+    
+    for asteroid in asteroids:
+        asteroid.y += asteroid.velocity
+        
+        if asteroid.y > height:
+            asteroids.remove(asteroid)
 
 
 def listen():
@@ -51,20 +83,15 @@ def listen():
 
 
 def setup_world():
-    # WIN.fill(BLUE)
+    global asteroids
+    
     WIN.blit(VOID, (0, 0))
 
     WIN.blit(ROBOT, (robot.x, robot.y))
-
-    #for testing
-    # obs = pygame.Rect(10, 10, OBS_WIDTH, OBS_HEIGHT)
-    # WIN.blit(ASTEROID_IMAGE, (obs.x, obs.y))
-    x = random.randint(0, 900)
-    obs = pygame.Rect(x, 0, OBS_WIDTH, OBS_HEIGHT)
-    WIN.blit(ASTEROID_IMAGE, (obs.x, obs.y))
-    # for rect in obstacles_list:
-    #     obs = pygame.Rect(rect[0], rect[1], OBS_WIDTH, OBS_HEIGHT)
-    #     WIN.blit(ASTEROID_IMAGE, rect)
+    
+    for asteroid in asteroids:
+        WIN.blit(asteroid.image, (asteroid.x, asteroid.y))
+    move_asteroids()
 
     pygame.display.update()
 
